68934807f143b6bfdc1c718f8e253e03a2802b27
unknown <anesed-0@student.ltu.se>
2024-09-12 17:01:28 +0200

    added some tests to for contact. Also changed docket-compose.yml so that a bootstrap node is created with a constant ip-address

--- kademlia/contact_test.go
+++ kademlia/contact_test.go
@@ -1,6 +1,9 @@
 package kademlia
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestNewContact(t *testing.T) {
 	// Check that contact with correct info is returned
@@ -49,16 +52,65 @@ func TestLess(t *testing.T) {
 }
 
 func TestString(t *testing.T) {
-	// Check that string functions as expected
+	// Check that String functions as expected
+	c := NewContact(NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8000")
+	s := c.String()
+	var sType interface{} = s
 
+	// test if the type is String
+	_, ok := sType.(string)
+
+	if !ok {
+		t.Fatalf("String() does not return a string")
+	}
+
+	// test that string contains expected result
+	s2 := `contact("ffffffff00000000000000000000000000000000", "localhost:8000")`
+
+	if s != s2 {
+		t.Fatalf("String do not match. %s != %s", s, s2)
+	}
 }
 
 func TestCandidatesAppend(t *testing.T) {
+	// Check that Append functions as expected
+	c1 := NewContact(NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8000")
+	c2 := NewContact(NewKademliaID("EFFFFFFF00000000000000000000000000000000"), "localhost:8000")
+	c3 := NewContact(NewKademliaID("DFFFFFFF00000000000000000000000000000000"), "localhost:8000")
 
+	cList := []Contact{c1, c2, c3}
+
+	var cc ContactCandidates
+
+	cc.Append(cList)
+
+	if cc.Len() != 3 {
+		t.Fatalf("ContactCandidates is of the wrong size!")
+	}
+
+	if !(cc.contacts[0] == c1 && cc.contacts[1] == c2 && cc.contacts[2] == c3) {
+		t.Fatalf("ContactCandidates does not contain the correct values! \nOriginal values = %v,\nSaved values = %v", &cList, cc.GetContacts(3))
+	}
 }
 
 func TestCandidatesGetContacts(t *testing.T) {
+	c1 := NewContact(NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8000")
+	c2 := NewContact(NewKademliaID("EFFFFFFF00000000000000000000000000000000"), "localhost:8000")
+	c3 := NewContact(NewKademliaID("DFFFFFFF00000000000000000000000000000000"), "localhost:8000")
 
+	cList := []Contact{c1, c2, c3}
+
+	var cc ContactCandidates
+
+	cc.Append(cList)
+
+	res := cc.GetContacts(1)
+	res2 := cc.GetContacts(2)
+	res3 := cc.GetContacts(3)
+
+	fmt.Printf("%v", res)
+	fmt.Printf("%v", res2)
+	fmt.Printf("%v", res3)
 }
 
 func TestCandidatesSort(t *testing.T) {
