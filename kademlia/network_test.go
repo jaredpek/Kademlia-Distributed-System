3faac7f8105588bceeb3a50f783665efd614708a
Sinoeng <simonspost2012@gmail.com>
2024-09-27 14:31:07 +0200

    Initial test

--- kademlia/network_test.go
+++ kademlia/network_test.go
@@ -4,19 +4,20 @@ import (
 	"testing"
 )
 
+var me = NewContact(NewRandomKademliaID(), "127.0.0.1:1234")
+var rt = NewRoutingTable(me)
+var n = Network{
+	ListenPort:        "1234",
+	PacketSize:        1024,
+	ExpectedResponses: make(map[KademliaID]chan Message, 10),
+	Rt:                rt,
+}
+
 func TestPingLocal(t *testing.T) {
-	me := NewContact(NewRandomKademliaID(), "127.0.0.1:1234")
-	rt := NewRoutingTable(me)
-	n := Network{
-		ListenPort:        "1234",
-		PacketSize:        1024,
-		ExpectedResponses: make(map[KademliaID]chan Message, 10),
-		Rt:                rt,
-	}
 	ch := make(chan Message, 5)
 
 	go n.Listen()
-	go n.SendPingMessage(&me, ch) // ping bootstrap
+	go n.SendPingMessage(&me, ch)
 	response := <-ch
 	if response.MsgType != "PONG" {
 		t.Fatalf("Received message was not of correct type")
