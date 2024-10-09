3e6d87ced0d6f9d7c0ce1eb449ee5abb6eea7d3b
jaredpek <83465527+jaredpek@users.noreply.github.com>
2024-10-09 14:47:54 +0200

    add functionality for lookup data (will have to split into functions later)

--- kademlia/kademlia.go
+++ kademlia/kademlia.go
@@ -82,7 +82,7 @@ func (kademlia *Kademlia) LookupContact(target KademliaID) []Contact {
 			// Receive the response from the channel
 			message := <-responses
 
-			// Print list of contacts
+			// Print list of contacts and add contact to routing table
 			log.Println("[FIND_CONTACT] Got contact response: ")
 			for _, foundContact := range message.Contacts {
 				sender := foundContact
@@ -136,9 +136,80 @@ func (kademlia *Kademlia) JoinNetwork() {
 // should return a string with the result. if the data could be found a string with the data and node it
 // was retrived from should be returned. otherwise just return that the file could not be found
 func (kademlia *Kademlia) LookupData(hash string) string {
-	// TODO
-	// return "The requested file could not be downloaded"
-	panic("LookupData not implemented")
+	log.Println("[FIND_DATA] Performing lookup data")
+	var closest ContactCandidates
+	var contacted map[string]bool = map[string]bool{}
+	responses := make(chan Message, 5)
+	id := NewKademliaID(hash)
+
+	// For each contact of the initial k-closest contacts to the target
+	for _, contact := range kademlia.Rt.FindClosestContacts(id, bucketSize) {
+		// Calculate the distance to the target
+		contact.CalcDistance(id)
+
+		// Create record for new contact
+		contacted[contact.Address] = false
+
+		// Add it to the closest list
+		closest.Append([]Contact{contact})
+	}
+
+	for {
+		var contacts []Contact
+
+		// Sort the contacts by their distance
+		closest.Sort()
+
+		// For each contact of the k-closest
+		for _, closestContact := range closest.GetContacts(bucketSize) {
+			// Continue to the next contact if already contacted
+			if contacted[closestContact.Address] {
+				continue
+			}
+
+			// Stop sending find contact requests if reached alpha nodoes
+			if len(contacts) >= Alpha {
+				break
+			}
+
+			// Send lookup data request to the node async
+			go kademlia.Network.SendFindDataMessage(*id, &closestContact, responses)
+
+			// Update contact record status
+			contacted[closestContact.Address] = true
+			contacts = append(contacts, closestContact)
+		}
+
+		// For each contact that was sent a find contact message
+		for i := 0; i < len(contacts); i++ {
+			// Receive the response from the channel
+			message := <-responses
+			if message.Body != "" {
+				return message.Body
+			}
+
+			// Add contacts to routing table
+			for _, foundContact := range message.Contacts {
+				sender := foundContact
+				sender.CalcDistance(kademlia.Rt.me.ID) // calc distance to self
+				go kademlia.Network.AddContact(sender)
+			}
+
+			// For each contact that was received from the message
+			for j := 0; j < len(message.Contacts); j++ {
+				// Calculate the distance between the target and the contact
+				message.Contacts[j].CalcDistance(id)
+			}
+
+			// Add the found contacts to the list of closest contacts
+			closest.Append(message.Contacts)
+		}
+
+		// If there are no k closest contacts that are uncontacted, return k closest contacts
+		if len(contacts) == 0 {
+			return "The requested file could not be downloaded"
+		}
+	}
 }
 
 // should return the hash of the data if it was successfully uploaded.
